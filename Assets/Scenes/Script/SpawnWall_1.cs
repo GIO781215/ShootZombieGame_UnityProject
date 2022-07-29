54511bbc534566bb24bb244e4613f727e1d34ca0
GIO781215 <gio.781215@gmail.com>
2022-07-29 20:46:55 +0800

    空氣牆暫存1

---
+++ Assets/Scenes/Script/SpawnWall_1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWall_1 : MonoBehaviour //空氣牆_1 的腳本
+{
+
+    [SerializeField] GameObject Zombie; //殭屍預製物件
+    [SerializeField] Transform[] spawnPoint; //要生成的位置 (有好幾個，都放在陣列裡)
+
+    float spawnTime = 3f; //每個殭屍生成的間隔時間
+    int spawnAmoint = 10; //殭屍要生成的總數量
+
+    bool hasBeenTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasBeenTrigger) return;
+
+        if(other.gameObject.tag == "Player")
+        {
+            hasBeenTrigger = true;
+            StartCoroutine(SpawnZombie());
+        }
+    }
+
+
+    IEnumerator SpawnZombie()
+    {
+        for(int i = 0; i < spawnAmoint; i++)
+        {
+            int index = Random.Range(0, spawnPoint.Length - 1);
+            Instantiate(Zombie, spawnPoint[index].position, spawnPoint[index].rotation);
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
+
+
+
+
+
+
+
+}
