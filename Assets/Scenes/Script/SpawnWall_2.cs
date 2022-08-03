95d0a31452b3c6dc3362bff73787b0b2278bb165
GIO781215 <gio.781215@gmail.com>
2022-08-03 23:54:36 +0800

    關卡調整OK

--- Assets/Scenes/Script/SpawnWall_2.cs
+++ Assets/Scenes/Script/SpawnWall_2.cs
@@ -19,7 +19,9 @@ public class SpawnWall_2 : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             hasBeenTrigger = true;
-            SpawnZombie();
+            SpawnZombie(); //在橋下生成一堆殭屍
+
+            StartCoroutine(SpawnZombieAlways()); //在固定3個地方一直生成殭屍
         }
     }
 
@@ -82,63 +84,29 @@ public class SpawnWall_2 : MonoBehaviour
 
         }
 
-
-
-
-
-
-
-
-
-
-
-
-        for (int i = 0; i < 33; i++)
-        {
-            float positionOffset_X = Random.Range(0, 30f) - 15f;
-            float positionOffset_Z = Random.Range(0, 30f) - 15f;
-            float angleOffset = Random.Range(0, 360f);
-
-            GameObject zombieTemp = Instantiate(Zombie, spawnPoint[4].position + new Vector3(positionOffset_X, 0, positionOffset_Z), Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("Scence_Prefab_")[0].transform); //生成殭屍
-            PatrolPath zombiePatrolPathTemp = Instantiate(ZombiePatrolPath, spawnPoint[4].position + new Vector3(positionOffset_X, 0, positionOffset_Z), Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("PatroPathCollection")[0].transform); //生成僵屍的巡邏路徑，第四個參數是生成的實體要把誰當作父對象
-            zombieTemp.GetComponent<ZombieController>().patrolPath = zombiePatrolPathTemp; //將巡邏路徑指定給殭屍
-            zombieTemp.GetComponent<ZombieController>().alwaysPatrol = true;
-            zombieTemp.GetComponent<ZombieController>().OnDamageIsChasing = true;
-            zombieTemp.GetComponent<ZombieController>().viewDistance= 10000;
-        }
-
-        for (int i = 0; i < 33; i++)
-        {
-            float positionOffset_X = Random.Range(0, 30f) - 15f;
-            float positionOffset_Z = Random.Range(0, 30f) - 15f;
-            float angleOffset = Random.Range(0, 360f);
-
-            GameObject zombieTemp = Instantiate(Zombie, spawnPoint[5].position + new Vector3(positionOffset_X, 0, positionOffset_Z), Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("Scence_Prefab_")[0].transform); //生成殭屍
-            PatrolPath zombiePatrolPathTemp = Instantiate(ZombiePatrolPath, spawnPoint[5].position + new Vector3(positionOffset_X, 0, positionOffset_Z), Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("PatroPathCollection")[0].transform); //生成僵屍的巡邏路徑，第四個參數是生成的實體要把誰當作父對象
-            zombieTemp.GetComponent<ZombieController>().patrolPath = zombiePatrolPathTemp; //將巡邏路徑指定給殭屍
-            zombieTemp.GetComponent<ZombieController>().alwaysPatrol = true;
-            zombieTemp.GetComponent<ZombieController>().OnDamageIsChasing = true;
-            zombieTemp.GetComponent<ZombieController>().viewDistance = 10000;
-        }
-
-        for (int i = 0; i < 33; i++)
-        {
-            float positionOffset_X = Random.Range(0, 30f) - 15f;
-            float positionOffset_Z = Random.Range(0, 30f) - 15f;
-            float angleOffset = Random.Range(0, 360f);
-
-            GameObject zombieTemp = Instantiate(Zombie, spawnPoint[6].position + new Vector3(positionOffset_X, 0, positionOffset_Z), Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("Scence_Prefab_")[0].transform); //生成殭屍
-            PatrolPath zombiePatrolPathTemp = Instantiate(ZombiePatrolPath, spawnPoint[6].position + new Vector3(positionOffset_X, 0, positionOffset_Z), Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("PatroPathCollection")[0].transform); //生成僵屍的巡邏路徑，第四個參數是生成的實體要把誰當作父對象
-            zombieTemp.GetComponent<ZombieController>().patrolPath = zombiePatrolPathTemp; //將巡邏路徑指定給殭屍
-            zombieTemp.GetComponent<ZombieController>().alwaysPatrol = true;
-            zombieTemp.GetComponent<ZombieController>().OnDamageIsChasing = true;
-            zombieTemp.GetComponent<ZombieController>().viewDistance = 10000;
-        }
-
-    
     }
 
- 
-     
+
+
+
+
+    IEnumerator SpawnZombieAlways()
+    {
+        while(true)
+        {
+            int index = Random.Range(4, 7); // Random.Range(int 最小值，int 最大) : 隨機產生一個整數，範圍是 最小值 ~ 最大值(不包含) ， Random.Rang(float 最小值，float 最大) : 隨機產生一個浮點數，範圍是 最小值 ~ 最大值(包含) 
+
+            float angleOffset = Random.Range(0, 360f);
+            GameObject zombieTemp = Instantiate(Zombie, spawnPoint[index].position, Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("Scence_Prefab_")[0].transform); //生成殭屍
+            PatrolPath zombiePatrolPathTemp = Instantiate(ZombiePatrolPath, spawnPoint[index].position, Quaternion.Euler(0, angleOffset, 0), GameObject.FindGameObjectsWithTag("PatroPathCollection")[0].transform); //生成僵屍的巡邏路徑，第四個參數是生成的實體要把誰當作父對象
+            zombieTemp.GetComponent<ZombieController>().patrolPath = zombiePatrolPathTemp; //將巡邏路徑指定給殭屍
+            zombieTemp.GetComponent<ZombieController>().alwaysPatrol = true;
+            zombieTemp.GetComponent<ZombieController>().OnDamageIsChasing = true;
+            zombieTemp.GetComponent<ZombieController>().viewDistance = 10000;
+
+            yield return new WaitForSeconds(10f);
+        }
+    }
+
 
 }
