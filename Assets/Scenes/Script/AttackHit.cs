0d02fe463c5564990a4040065d4d0b328e4b7246
GIO781215 <gio.781215@gmail.com>
2022-08-11 20:35:52 +0800

    殭屍射擊遊戲完成~

--- Assets/Scenes/Script/AttackHit.cs
+++ Assets/Scenes/Script/AttackHit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class AttackHit : MonoBehaviour //此腳本會掛在能攻擊的角色的 FBX 模型下，作為角色的 FBX 模型執行攻擊動畫時會呼叫的函數
 {
     void Hit() //當角色做出攻擊動作此函數被觸發時，會直接呼叫掛載在父物件角色下的 角色_Controller 腳本中的 AtHit() 函數
